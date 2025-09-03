975c80f1102ebcf451a7b3628be566da65649e9b
sirgowtham2428-ops <sirgowtham2428@gamil.com>
2025-09-03 11:00:49 +0530

    day1 finised

---
+++ Day-1-project.py
@@ -0,0 +1,6 @@
+### building a brand name generator 
+
+print("Welcome to the brand name generator !!! ")
+city=input("In which city you grow  up in ? ")
+pet=input("What is the name of your pet ? ")
+print(f"Your brand name is {city +  pet} ")
